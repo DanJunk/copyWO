343eff75880001c6f19b1566828979d5a4a39341
Dan Junk <105869607+DanJunk@users.noreply.github.com>
2023-05-03 01:07:27 +0200

    Create script.user.js

---
+++ copyWO/script.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         CopyWO
+// @namespace    https://github.com/DanJunk/copyWO/
+// @version      1.0.0
+// @description  
+// @author       danJunk
+// @match        https://t.corp.amazon.com/*
+// @icon         
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @resource     STYLE1 https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/style.css
+// @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/selector.js
+// ==/UserScript==
+
+GM_addStyle (GM_getResourceText("STYLE1"));
