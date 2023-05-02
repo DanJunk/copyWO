9cbc47900a3d00241aaf42f4bc95a49ae9cdcb8f
Dan Junk <105869607+DanJunk@users.noreply.github.com>
2023-05-03 01:12:28 +0200

    Update script.user.js

--- copyWO/script.user.js
+++ copyWO/script.user.js
@@ -2,14 +2,14 @@
 // @name         CopyWO
 // @namespace    https://github.com/DanJunk/copyWO/
 // @version      1.0.0
-// @description  
+// @description  Copy the Workorder Number right out of the Ticket
 // @author       danJunk
 // @match        https://t.corp.amazon.com/*
-// @icon         
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
-// @resource     STYLE1 https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/style.css
-// @require      https://raw.githubusercontent.com/dev-101010/APMOD/main/APMOD/inject/selector.js
+// @resource     STYLE1 https://raw.githubusercontent.com/DanJunk/copyWO/main/copyWO/scripts/style.css
+// @require      https://raw.githubusercontent.com/DanJunk/copyWO/main/copyWO/scripts/content.js
 // ==/UserScript==
 
 GM_addStyle (GM_getResourceText("STYLE1"));
